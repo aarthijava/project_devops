17fda28c02adae846dc17ed04fc8806d4eb83f8c
aarthijava <aarthi.devops@gmail.com>
2022-05-04 12:28:11 +0530

    ebsec2

--- terraform/ebsattach.tf
+++ terraform/ebsattach.tf
@@ -1,10 +1,6 @@
-/*
+
 resource "aws_volume_attachment" "ebs_attch" {
-  device_name = "/dev/xvf"
-  volume_id   = vol-02c5ceef10164c749
-  instance_id = i-0d8376ba9f9d88f7a
+  device_name = "/dev/xvdf"
+  volume_id   = "vol-03fb8e298f042fd74"
+  instance_id = "i-023b735b3062d31c4"
 }
-
-
-
-*/
--- terraform/ec2ebs.tf
+++ terraform/ec2ebs.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "ec2ebsdemo" {
   ami = "ami-0c04fae3632224f24"
   instance_type = "t2.micro"
   key_name = "hellous"
+  availability_zone = "us-west-2a"
   tags = {
     Name="ec2ebs"
     server="web"
